c9203e5a633c1654bb30a856e98c3b24ed690fd3
Bijayananda Mohanta <bijayananda.dev@colourmoon.com>
2025-09-27 08:46:32 +0530

    minify the images and added one new project, with that removed the interesting part concept.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// script.js - Main JavaScript file for portfolio site
+
 $(document).ready(function () {
+  // Initialize Magnific Popup for image links
   $(".popup-link").magnificPopup({
     type: "image",
     closeOnContentClick: true,
@@ -8,48 +11,56 @@ $(document).ready(function () {
       verticalFit: true,
     },
   });
-});
 
-$(document).on("click", ".question", function () {
-  $(".answer").hide();
-  let answerHtml = $(this).next(".answer").html();
+  // FAQ Interaction Handlers
+  $(document).on("click", ".question", function () {
+    $(".answer").hide();
+    const answerHtml = $(this).next(".answer").html();
 
-  $(".answer-view")
-    .stop(true, true)
-    .fadeOut(150, function () {
-      $(this).html(
-        '<button class="close-answer"><i class="bi bi-x"></i></button><div>' +
-          answerHtml +
-          "</div>"
-      );
-      $(this).fadeIn(250);
+    $(".answer-view")
+      .stop(true, true)
+      .fadeOut(150, function () {
+        $(this).html(
+          '<button class="close-answer"><i class="bi bi-x"></i></button><div>' +
+            answerHtml +
+            "</div>"
+        );
+        $(this).fadeIn(250);
+      });
+  });
+
+  $(document).on("click", ".close-answer", function (e) {
+    e.stopPropagation();
+    $(".answer-view").fadeOut(250, function () {
+      $(this).empty();
     });
-});
-
-$(document).on("click", ".close-answer", function (e) {
-  e.stopPropagation();
-  $(".answer-view").fadeOut(250, function () {
-    $(this).empty();
   });
 });
 
-// Whatsapp redirection format
-var $wa = $("#wa-link");
-var desktopLink = $wa.data("desktop-link");
-var mobileLink = $wa.attr("href");
-function isMobileUA() {
-  return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
-    navigator.userAgent
-  );
+// WhatsApp Redirection Logic
+function setupWhatsAppRedirection() {
+  const $wa = $("#wa-link");
+  const desktopLink = $wa.data("desktop-link");
+  const mobileLink = $wa.attr("href");
+
+  function isMobileUA() {
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+      navigator.userAgent
+    );
+  }
+
+  // Screen width check in rem converted to px (40rem * 16 = 640px)
+  function isSmallScreen() {
+    return window.matchMedia("(max-width: 40rem)").matches;
+  }
+
+  // If user NOT on mobile user agent AND screen width is NOT less than 40rem => desktop
+  if (!isMobileUA() && !isSmallScreen()) {
+    $wa.attr("href", desktopLink);
+  } else {
+    $wa.attr("href", mobileLink);
+  }
 }
 
-// Screen width check in rem converted to px (40rem * 16 = 640px)
-function isSmallScreen() {
-  return window.matchMedia("(max-width: 40rem)").matches;
-}
-// If user NOT on mobile user agent AND screen width is NOT less than 40rem => desktop
-if (!isMobileUA() && !isSmallScreen()) {
-  $wa.attr("href", desktopLink);
-} else {
-  $wa.attr("href", mobileLink);
-}
+// Initialize WhatsApp redirection on load
+setupWhatsAppRedirection();
